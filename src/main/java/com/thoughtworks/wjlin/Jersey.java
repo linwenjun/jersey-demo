ce0843c8dd34863d0de70c374201890fd66e1cce
Wenjun LIN <linwenjun@163.com>
2015-11-25 18:55:12 +0800

    Create a response formatted by json

--- src/main/java/com/thoughtworks/wjlin/Jersey.java
+++ src/main/java/com/thoughtworks/wjlin/Jersey.java
@@ -1,17 +1,32 @@
 package com.thoughtworks.wjlin;
 
+import com.thoughtworks.wjlin.bean.Car;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/api")
 public class Jersey {
 
     @GET
-    @Path("/{param}")
+    @Path("/plain/{param}")
     public Response getMsg(@PathParam("param") String msg) {
         String output = "Jersey say: " + msg;
         return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Car getCar() {
+        Car car = new Car();
+        car.setColor("red");
+        car.setCount(10);
+
+        return car;
     }
 }
---
+++ src/main/java/com/thoughtworks/wjlin/bean/Car.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.wjlin.bean;
+
+public class Car {
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    private String color;
+    private int count;
+}
